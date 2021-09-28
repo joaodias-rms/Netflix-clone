ccf6c888211ecbccfc97e0cd787ce4d8163e497d
joao <jvd.ramos@hotmail.com>
2021-09-28 17:22:15 -0400

    component stylization

---
+++ src/components/Header/styles.js
@@ -0,0 +1,23 @@
+import {StyleSheet} from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    position: 'absolute',
+    top: 0,
+    flex: 1
+  },
+  headerSafeAreaView: {
+      alignItems: 'center',
+      flexDirection: 'row',
+      justifyContent: 'space-evenly',
+      width: '100%'
+  },
+  logoHeader: {
+    height: 50,
+    width: 50,
+  },
+  text: {
+    color: '#fff',
+  },
+
+});
---
+++ src/components/Hero/styles.js
@@ -0,0 +1,10 @@
+import {StyleSheet} from 'react-native'
+
+export const styles = StyleSheet.create({
+   container:{},
+   hero:{
+       height: 400,
+       width: '100%'
+   }
+
+})
