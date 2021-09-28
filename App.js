af50b8c71a12afabcd7cc389855940edb4194f31
joao <jvd.ramos@hotmail.com>
2021-09-28 15:25:12 -0400

    add primary project colors

--- App.js
+++ App.js
@@ -1,10 +1,23 @@
 import React from 'react'
 import { View, Text } from 'react-native'
+import {DefaultTheme, Provider as PaperProvider} from 'react-native-paper'
+import { Login } from './src/screens/Login';
 
 export default function App() {
+  const theme = {
+    ...DefaultTheme,
+    colors:{
+      ...DefaultTheme.colors,
+      primary: "#e31e20",
+      background: '#3c3c3c',
+      placeholder: '#fff',
+      text: '#fff'
+    }
+  }
+
   return (
-    <View>
-      <Text></Text>
-    </View>
+    <PaperProvider theme={theme}>
+      <Login />
+    </PaperProvider>
   )
 }
