61c023dc918c6f152f8e421d32a99cca0c8e706e
joao <jvd.ramos@hotmail.com>
2021-09-28 14:50:33 -0400

    refactor main index

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
-/**
- * @format
- */
-
 import {AppRegistry} from 'react-native';
 import App from './App';
+import { Login } from './src/screens/Login';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => Login);
