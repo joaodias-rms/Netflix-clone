45325177459532e82c204c723f6b54053938f218
joao <jvd.ramos@hotmail.com>
2021-09-28 15:25:30 -0400

    changed register component main index

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
-import { Login } from './src/screens/Login';
+
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => Login);
+AppRegistry.registerComponent(appName, () => App);
