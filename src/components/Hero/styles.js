3fa6ef981b53e58bb604bf523a9bae9d450e9bcb
joao <jvd.ramos@hotmail.com>
2021-09-29 08:55:27 -0400

    component stylization

--- src/components/Hero/styles.js
+++ src/components/Hero/styles.js
@@ -1,10 +1,44 @@
 import {StyleSheet} from 'react-native'
 
 export const styles = StyleSheet.create({
-   container:{},
    hero:{
        height: 400,
        width: '100%'
+   },
+   imgLogo:{
+       height: 150,
+       width: '100%',
+       marginTop: 200,
+       alignSelf: 'center',
+       position: 'absolute',
+       zIndex: 10,
+   },
+   top10Container:{
+    position: 'absolute',
+    zIndex: 10,
+    bottom: 20,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    width: "100%",
+    alignItems: 'center'
+   },
+   top10Logo:{
+       height: 40,
+       width: 40,
+       backgroundColor: '#fff',
+       marginRight: 15,
+       borderRadius: 40
+   },
+   top10Text:{
+    color: '#fff',
+    fontSize: 15
+   },
+   gradient:{
+       width: '100%',
+       height: 150,
+       position: 'absolute',
+       zIndex: 9,
+       bottom: 0
    }
 
 })
