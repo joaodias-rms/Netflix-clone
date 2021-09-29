9351f16ac58ee41becb6b749f994ddba9b382321
joao <jvd.ramos@hotmail.com>
2021-09-29 08:56:17 -0400

    added badge img in the hero component

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,11 +1,29 @@
 import React from 'react';
-import {View, Text, ImageBackground} from 'react-native';
+import {View, Text, ImageBackground, Image} from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
+
+import top10 from '../../assets/top10.png';
+
 import {styles} from './styles';
 
 export function Hero() {
   return (
     <ImageBackground
       style={styles.hero}
-      source={{uri: 'https://i.imgur.com/EJyDFeY.jpg'}}></ImageBackground>
+      source={{uri: 'https://i.imgur.com/EJyDFeY.jpg'}}>
+      <Image
+        style={styles.imgLogo}
+        resizeMode="contain"
+        source={{uri: 'https://i.imgur.com/4xN7wB8.png'}}
+      />
+
+      <View style={styles.top10Container}>
+        <Image  style={styles.top10Logo} source={top10} />
+        <Text style={styles.top10Text}>Top 1 hoje no Brasil.</Text>
+      </View>
+      <LinearGradient
+      style={styles.gradient}
+      colors={['rgba(0,0,0,0)', 'rgba(0,0,0,0.7)']} />
+    </ImageBackground>
   );
 }
