c1db98fdb546765694cc4ae84ea60a141bd643b3
joao <jvd.ramos@hotmail.com>
2021-09-30 15:32:12 -0400

    component stylization

---
+++ src/components/Episodes/styles.js
@@ -0,0 +1,19 @@
+import {StyleSheet} from 'react-native'
+
+export const styles = StyleSheet.create({
+   container:{
+    marginBottom: 20
+   },
+   imageContainer:{
+       flexDirection: 'row',
+   },
+   image:{
+       width: 130,
+       height: 70,
+       marginRight: 10
+   },
+   title:{
+       fontSize: 15
+   }
+
+})
--- src/screens/Movies/styles.js
+++ src/screens/Movies/styles.js
@@ -1,6 +1,46 @@
-import {StyleSheet} from 'react-native'
+import {StyleSheet} from 'react-native';
 
 export const styles = StyleSheet.create({
-   container:{}
-
-})
+  container: {
+    flex: 1,
+    backgroundColor: '#000',
+  },
+  hero: {
+    height: 200,
+    width: '100%',
+  },
+  movieDataContainer: {
+    padding: 20,
+  },
+  watchButton: {
+    marginVertical: 20,
+  },
+  captionInfo: {
+    marginVertical: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  captionCast: {
+    color: '#fff',
+  },
+  buttonList: {
+    marginVertical: 20,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  seasonButton: {
+    width: '100%',
+    backgroundColor: 'rgba(255,255,255,0.08)',
+    borderWidth: 1,
+    borderColor: 'rgba(255,255,255, 0.21)',
+    borderRadius: 3,
+    padding: 10,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginVertical: 20
+  },
+  seasonName: {
+      color: '#fff'
+  },
+});
