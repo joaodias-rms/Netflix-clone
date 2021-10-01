47cca6829a8a8dae70ae0ad814d90fe596276e7d
joao <jvd.ramos@hotmail.com>
2021-10-01 11:18:32 -0400

    created tab navigation

---
+++ src/routes/tabs.routes.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import {createMaterialBottomTabNavigator} from '@react-navigation/material-bottom-tabs';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+
+import {Home} from '../screens/Home';
+
+export function TabsRoutes() {
+  const Tab = createMaterialBottomTabNavigator();
+
+  return (
+    <Tab.Navigator
+      initialRouteName="Home"
+      activeColor={'#fff'}
+      shifting={false}
+      barStyle={{backgroundColor: '#141414'}}>
+      <Tab.Screen
+        name="Home"
+        component={Home}
+        options={{
+          tabBarLabel: 'Home',
+          tabBarIcon: ({color}) => (
+            <Icon color={color} size={26} name="home" />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Search"
+        component={Home}
+        options={{
+          tabBarLabel: 'Buscar',
+          tabBarIcon: ({color}) => (
+            <Icon color={color} size={26} name="magnify" />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Coming"
+        component={Home}
+        options={{
+          tabBarLabel: 'Em Breve',
+          tabBarIcon: ({color}) => (
+            <Icon color={color} size={26} name="play-speed" />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Downloads"
+        component={Home}
+        options={{
+          tabBarLabel: 'Donwloads',
+          tabBarIcon: ({color}) => (
+            <Icon color={color} size={26} name="download" />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="More"
+        component={Home}
+        options={{
+          tabBarLabel: 'Mais',
+          tabBarIcon: ({color}) => <Icon color={color} size={26} name="menu" />,
+        }}
+      />
+    </Tab.Navigator>
+  );
+}
