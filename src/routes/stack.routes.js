8059e141fd01bc68c392f09de85f6c0c2b4a17cd
joao <jvd.ramos@hotmail.com>
2021-10-01 11:18:44 -0400

    created other stack navigations

--- src/routes/stack.routes.js
+++ src/routes/stack.routes.js
@@ -1,17 +1,31 @@
 import React from 'react';
 import {View, Text} from 'react-native';
-import { Login } from '../screens/Login';
-
 import {createStackNavigator} from '@react-navigation/stack';
+
+import {Login} from '../screens/Login';
+import {Movies} from '../screens/Movies';
+
+import {TabsRoutes} from './tabs.routes';
 
 export function StackRoutes() {
   const Stack = createStackNavigator();
   return (
-    <Stack.Navigator>
-        <Stack.Screen
-            name="Login"
-            component={Login}
-        />
+    <Stack.Navigator initialRouteName="Login">
+      <Stack.Screen
+        name="Login"
+        component={Login}
+        options={{headerShown: false}}
+      />
+      <Stack.Screen
+        name="Home"
+        component={TabsRoutes}
+        options={{headerShown: false}}
+      />
+      <Stack.Screen
+        name="Movies"
+        component={Movies}
+        options={{headerShown: false}}
+      />
     </Stack.Navigator>
   );
 }
