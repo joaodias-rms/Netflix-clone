718b6182b051def812c2e232d1e170992fd988ec
joao <jvd.ramos@hotmail.com>
2021-10-06 11:30:11 -0400

    getting movies from tmdb api

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import {View, ScrollView} from 'react-native';
+import React, {useEffect} from 'react';
+import {View, ScrollView, Alert} from 'react-native';
 import {Button, Title} from 'react-native-paper';
 
 import {ButtonVertical} from '../../components/ButtonVertical';
@@ -8,9 +8,33 @@ import {Hero} from '../../components/Hero';
 import {Preview} from '../../components/Preview';
 import {Section} from '../../components/Section';
 
+import api from '../../services/api';
+
+const {APIKEY} = process.env;
+
 import {styles} from './styles';
 
 export function Home() {
+  const getMovie = async () => {
+    try {
+      const movies = await api.get(
+        `${api.defaults.baseURL}/discover/movie?api_key=${APIKEY}`,
+      );
+      const res = movies.data.results
+
+
+      if(res.error){
+        Alert(error.message)
+      }
+    } catch (error) {
+      Alert(error.message);
+    }
+  };
+
+  useEffect(() => {
+    getMovie();
+  }, []);
+
   return (
     <ScrollView style={styles.container}>
       <Hero />
@@ -29,7 +53,7 @@ export function Home() {
         <Preview />
       </View>
       {[1, 2, 3, 4].map((section, index) => (
-        <Section key={index}title="Novidades" />
+        <Section key={index} title="Novidades" />
       ))}
     </ScrollView>
   );
